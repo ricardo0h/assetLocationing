bfdf462461658d8e4fa97e05eab41c75235dc071
Miska Melkinen <miska.melkinen@student.tut.fi>
2017-02-28 12:56:54 +0200

    making node.js server

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const express = require('express')
+
+
+
+const app = express()
+const http = require('http').Server(app);
+
+
+const port = 3000
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+
+
+  console.log(`server is listening on ${port}`)
+})
